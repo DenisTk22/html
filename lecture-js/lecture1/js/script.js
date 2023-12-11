ce80f152a70ed18ad085c8e07c6627ab79e80662
Denis Tkachev <allpost-s@ya.ru>
2023-12-11 23:33:55 +0300

    javascript start

---
+++ lecture-js/lecture1/js/script.js
@@ -0,0 +1,43 @@
+
+// 'use strict';
+let countClickButton = 0;
+// console.log(typeof(countClickButton));
+countClickButton = countClickButton + 2;
+
+let num;
+console.log("переменная num имеет тип данных: " + typeof(num));
+
+let result = '8';
+console.log("переменная со значением '8' имеет тип данных: " + typeof(result));
+
+let result11 = 148 / 0;
+console.log("переменная со значением '148 / 0' имеет тип данных: " + typeof(result11));
+
+
+console.log("не определенная переменная имеет тип данных: " + typeof (result2));
+
+console.log(typeof(result));
+let liter = `Результат: ${result}`;
+console.log(liter);
+
+age = 40;
+console.log(age);
+
+console.log(`Нажали на кнопку ${result * 5} раз(а)`);
+
+let isPassExam = true;
+
+console.log('Я JS')
+console.log('Мы JS')
+
+console.log('Мы JS');
+console.log('Мы JS');
+
+const userProfile = {
+    name: 'Денис',
+    age: '40',
+    message: 'Все сбудется!'
+};
+
+console.log("мальчик " + userProfile.name + " , " + userProfile.age + " годиков.");
+
