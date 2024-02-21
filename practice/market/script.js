9b71d0d86e9371509c77fc6ce885407283dfb3ac
Denis Tkachev <allpost-s@ya.ru>
2024-02-21 13:57:53 +0300

    hw7

--- practice/market/script.js
+++ practice/market/script.js
@@ -1,3 +1,68 @@
+const dataProducts = `[
+    {
+        "id": "1",
+        "title": "ELLERY X M'O",
+        "subtitle": "CAPSULE",
+        "price": "$300",
+        "color": "Red",
+        "size": "XL",
+        "quantity": "1",
+        "photo": "./pictures/pic_id1.svg"
+    },
+    {
+        "id": "2",
+        "title": "ELLERY X M'O",
+        "subtitle": "CAPSULE",
+        "price": "$52",
+        "color": "Black",
+        "size": "XL",
+        "quantity": "1",
+        "photo": "./pictures/pic_id2.svg"
+    },
+    {
+        "id": "3",
+        "title": "ELLERY X M'O",
+        "subtitle": "CAPSULE",
+        "price": "$300",
+        "color": "Red",
+        "size": "XL",
+        "quantity": "1",
+        "photo": "./pictures/pic_id1.svg"
+    },
+    {
+        "id": "4",
+        "title": "ELLERY X M'O",
+        "subtitle": "CAPSULE",
+        "price": "$300",
+        "color": "Red",
+        "size": "XL",
+        "quantity": "1",
+        "photo": "./pictures/pic_id1.svg"
+    },
+    {
+        "id": "5",
+        "title": "ELLERY X M'O",
+        "subtitle": "CAPSULE",
+        "price": "$300",
+        "color": "Red",
+        "size": "XL",
+        "quantity": "1",
+        "photo": "./pictures/pic_id1.svg"
+    },
+    {
+        "id": "6",
+        "title": "ELLERY X M'O",
+        "subtitle": "CAPSULE",
+        "price": "$300",
+        "color": "Red",
+        "size": "XL",
+        "quantity": "1",
+        "photo": "./pictures/pic_id1.svg"
+    },
+]`
+
+const data = JSON.parse(dataProducts);
+
 
 const productCardEl = document.querySelector('.products_card');
 
