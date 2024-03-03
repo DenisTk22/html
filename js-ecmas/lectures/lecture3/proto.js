20cccc9b5e7af90e7c596726fee1e6dee16ebe69
Denis Tkachev <allpost-s@ya.ru>
2024-03-03 21:10:09 +0300

    lecture3

---
+++ js-ecmas/lectures/lecture3/proto.js
@@ -0,0 +1,85 @@
+//Прототипы
+//Листинг 2.
+// Объявление родительского объекта смотри в листинге 1.
+// Объект робот-пылесос.
+const DancingSeries = {
+    // Объявляем новые свойства и переопределить свойство model.
+    model: "dancing series",
+    power: 200,
+    batterySize: 2100,
+    boxSize: 0.5,
+    workTime: 45,
+    isUVLampOn: false,
+    // Добавляем новый метод.
+    switchUVLamp: function () {
+// Добавим дополнительный вывод, чтобы знать чей метод мы вызвали.
+        console.log('I am the method of DancingSeries');
+        this.isUVLampOn = !this.isUVLampOn;
+        console.log(`UV lamp is ${this.isUVLampOn ? 'working' :
+            'not working'}.`);
+    },
+    // Делаем ссылку на прототип от родителя.
+    __proto__: VacuumCleaner,
+};
+
+//Листинг 3.
+// Объявление базового родительского объекта смотри в листинге
+// Объявление DancingSeries смотри в листинге 2.
+// Объект робот-пылесос.
+const Samba = {
+    // Обновляем свойства под конкретную модель.
+    model: "Samba-1",
+    power: 250,
+    batterySize: 2500,
+    workTime: 50,
+    // Делаем ссылку на прототип от родителя.
+    __proto__: DancingSeries,
+};
+
+//Листинг 4.
+// Обращение к свойствам объекта.
+console.log(Samba.model); // "Samba-1"
+console.log(Samba.isFull); // false
+// Вызов методов объекта.
+Samba.startCleaning();
+// I am the method of VacuumCleaner
+// 'I am cleaning... I have been started: 1 times.'
+Samba.isUVLampOn = true;
+Samba.switchUVLamp();
+// I am the method of DancingSeries
+// 'UV lamp is not working.'
+Samba.goCharge();
+// I am the method of VacuumCleaner
+// 'I am going to charge...'
+
+//Листинг 5.
+// Объект робот-пылесос.
+const Djaiv = {
+    // Обновляем свойства под конкретную модель.
+    model: "Djaiv-1",
+    power: 250,
+    batterySize: 2500,
+    workTime: 50,
+    // Переопределим метод startCleaning.
+    startCleaning: function () {
+        this.counterOfStarts++;
+// Добавим дополнительный вывод, чтобы знать чей метод мы вызвали.
+            console.log('I am the method of Djaiv');
+        console.log('I am Djaiv, and I am cleaning... I have been started: ', this.counterOfStarts, 'times.');
+},
+    // Делаем ссылку на прототип от родителя.
+    __proto__: DancingSeries,
+};
+
+//Листинг 6.
+// Объявление базового родительского объекта смотри в листинге
+// Объявление DancingSeries смотри в листинге 2.
+// Объект Samba смотри в листинге 3.
+// Объект Djaiv смотри в листинге 5.
+// Вызов методов объекта.
+Samba.startCleaning();
+// I am the method of VacuumCleaner
+// 'I am cleaning... I have been started: 1 times.'
+Djaiv.startCleaning();
+// I am the method of Djaiv
+// I am Djaiv, and I am cleaning... I have been started: 1 times.
