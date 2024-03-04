bf4f5a7b4e3de785ac42a964daf9cb5a19ba168d
Denis Tkachev <allpost-s@ya.ru>
2024-03-04 20:30:48 +0300

    Lecture3

---
+++ js-ecmas/lectures/lecture3/getproto.js
@@ -0,0 +1,100 @@
+//Методы для установки прототипа.
+// Устанавливать прототип объекта можно используя свойство __proto__, но также в
+// языке есть два метода для чтения и установки прототипа объекта - это
+// getPrototypeOf и setPrototypeOf.Эти методы не доступны в браузере Internet
+// Explorer версии ниже 10.
+// getPrototypeOf
+// Метод getPrototypeOf позволяет получить ссылку на объект прототип.Давайте
+// узнаем какой объект является прототипом для нашего объекта Djaiv, потом
+// посмотрим кто является его прототипом и кто является прототипом его прототипа:
+
+// Листинг 9.
+// Получим прототип для объекта Djaiv.
+const DjaivProto = Object.getPrototypeOf(Djaiv);
+console.log(DjaivProto.model); // dancing series
+
+const DjaivProtoProto = Object.getPrototypeOf(DjaivProto);
+console.log(DjaivProtoProto.model); //vacuum cleaner
+
+const DjaivProtoProtoProto = Object.getPrototypeOf(DjaivProtoProto);
+console.log(DjaivProtoProtoProto); // [object Object]
+// В последнем прототипе мы не стали смотреть свойство model, т.к.там его нет, мы
+// добрались до самого высокого родителя, которым является объект Object, все
+// объекты наследуются от него.Если мы попытаемся получить его прототип, то в
+// ответ получим null, т.к.Объект Object не имеет прототипа.
+//     setPrototypeOf
+// Зачем может понадобиться знать прототип объекта ? Давайте создадим другую
+// серию роботов - musicSeries, с немного другим функционалом(они будут уметь
+// мыть полы), и установим её в качестве прототипа для нашего нового пылесоса
+// Blues.Так как новая серия будет иметь дополнительный функционал, которого нет в
+// серии DancingSeries, прежде чем вызвать такой функционал у определенного
+// объекта, стоит проверить, кто его родитель.Для установки прототипа используем
+// метод setPrototypeOf - он принимает два аргумента, первый это объект для которого устанавливается прототип, второй - это объект который будет прототипом
+// для первого.
+// Для начала создадим объект серии:
+
+// Листинг 10.
+// Объект робот-пылесос.
+const MusicSeries = {
+    // Объявляем новые свойства и переопределяем свойство model.
+    model: "music series",
+    power: 200,
+    batterySize: 2100,
+    boxSize: 0.5,
+    workTime: 45,
+
+    // Добавляем новый метод.
+    startWipe: function () {
+// Добавим дополнительный вывод, чтобы знать чей метод мы вызвали.
+        console.log('I am the method of MusicSeries');
+        console.log('I am starting to wipe the floor...');
+    },
+    // Делаем ссылку на прототип от родителя.
+    __proto__: VacuumCleaner,
+};
+// Объект робот-пылесос.
+const MusicSeries1 = {
+    // Объявляем новые свойства и переопределяем свойство model.
+    model: "music series",
+    power: 200,
+    batterySize: 2100,
+    boxSize: 0.5,
+    workTime: 45,
+    // Добавляем новый метод.
+    startWipe: function () {
+// Добавим дополнительный вывод, чтобы знать чей метод мы вызвали.
+            console.log('I am the method of MusicSeries');
+        console.log('I am starting to wipe the floor...');
+    },
+    // Делаем ссылку на прототип от родителя.
+    __proto__: VacuumCleaner,
+};
+// Создадим нашего нового робота:
+// Листинг 11.
+// Объект робот-пылесос.
+const Blues = {
+    // Обновляем свойства под конкретную модель.
+    model: "Bluees-1",
+    power: 250,
+    batterySize: 2500,
+    workTime: 50,
+};
+// Установим прототип для робота.
+Object.setPrototypeOf(Blues, MusicSeries); // назначаем прототипы для Blues от MusicSeries
+// Теперь можем попробовать вызвать методы наших роботов, проверяя кто является
+// их прототипом:
+// Листинг 12.
+// Объявление базового родительского объекта смотри в листинге
+1.
+// Объявление DancingSeries смотри в листинге 2.
+// Объект Djaiv смотри в листинге 5.
+// Объявление MusicSeries смотри в листинге 10.
+// Объект Blues смотри в листинге 11.
+if (Object.getPrototypeOf(Djaiv).model === 'dancing series') {
+    Djaiv.startCleaning(); //
+}
+if (Object.getPrototypeOf(Blues).model === 'music series') {
+    Blues.startWipe(); //
+}
+// Если мы не будем проверять прототип и просто вызовем метод чужого прототипа мы, естественно, получим ошибку.
+Djaiv.startWipe(); // Uncaught TypeError: Djaiv.startWipe is not a function
---
+++ js-ecmas/lectures/lecture3/new.js
@@ -0,0 +1,83 @@
+// Оператор new
+//     Оператор new позволяет создавать новые объекты, используя для этого
+// функцию - конструктор.Работает он следующим образом:
+// 1. Создает пустой объект, который наполнит всем необходимым.
+// 2. Устанавливает этот объект как this для функции конструктора, чтобы можно
+// было использовать this внутри функции и добавлять свойства и методы в
+// этот объект.
+// 3. Вызывает функцию конструктор для инициализации объекта.
+// 4. Если у функции конструктора есть свойство prototype, устанавливает
+// значение этого свойства как прототип для нового объекта(свойство
+// __proto__).
+// 5. Устанавливает свойство constructor объекта ссылкой на функцию
+// конструктор.
+// 6. Если функция конструктор не возвращает ничего или возвращает какое - то
+// примитивное значение, то оператор new вернет новый созданный и
+// наполненный объект, если конструктор возвращает объект, то оператор new
+//     вернет этот объект.
+// Не так сложно, давайте попробуем создать свою версию оператора new в виде
+// функции:
+// Листинг 16.
+
+// Наша реализация оператора new через функцию createObject.
+function createObject(constructor) {
+    // Создаем новый объект.
+    const obj = {};
+    // Установим новому объекту прототипом прототип
+    функции - конструктора
+    Object.setPrototypeOf(obj, constructor.prototype);
+// Вызовем функцию-конструктор, передав ей как this созданный на шаге 1 объект, и передадим остальные аргументы,если они были переданы в createObject
+    const argsArray = Array.prototype.slice.apply(arguments);
+    const result = constructor.apply(obj, argsArray.slice(1));
+
+// Вернем новый объект, если конструктор вернул примитивное значение или undefined, иначе вернем то, что вернул конструктор.
+if(!result || typeof result === 'string' || typeof result
+            === 'number' || typeof result === 'boolean') {
+        return obj
+    } else {
+        return result;
+    }
+}
+
+// Создадим экземпляр нового объекта.
+const Samba1 = createObject(Samba, 1014778);
+// Проверим установку свойств в конструкторе.
+console.log(Samba1.serialNumber); // 1014778
+// Проверим, что прототип установился корректно, и мы можем вызывать методы из родительских объектов.
+console.log(Samba1.__proto__); // {model: "dancing series", power: 200, batterySize: 2100, boxSize: 0.5, workTime: 45, ...}
+console.log(Samba1.startCleaning()); // I am the method of
+VacuumCleaner
+// I am cleaning... I have been started: 1 times.
+// Проверим присвоение конструктора.
+console.log(Samba1.constructor); // function Object() { [native code] }
+
+// ❗Свойство __proto__ объекта и свойство prototype у функции
+// конструктора это не одно и то же.Свойство __proto__ есть у
+// экземпляра объекта, и оно позволяет находить родителей объекта,
+//     свойство prototype выполняет служебную функцию при создании
+// экземпляра объекта через оператор new.
+//     Посмотрите на пример кода, который расставит точки на «i».
+
+// Конструктор объекта робот-пылесос.
+function Samba(serailNumber) {
+    // Создаем свойства объекта, используя this.
+    this.serialNumber = serailNumber;
+    this.model = "Samba-1";
+    this.power = 250;
+    this.batterySize = 2500;
+    this.workTime = 50;
+}
+
+// Делаем ссылку на прототип от родителя.
+Samba.prototype = DancingSeries;
+
+// Создадим экземпляр нового объекта.
+const Samba2 = new Samba(1014778);
+
+// Посмотрим на свойства __proto__ и prototype
+console.log(Samba2.__proto__); // {model: "dancing series", power: 200, batterySize: 2100, boxSize: 0.5, workTime: 45, ...}
+
+console.log(Samba2.prototype); // undefined
+console.log(Samba2.__proto__ === Samba.prototype); // true
+// ❗ В экземпляра объекта нет свойства prototype, данные из него перешли в
+// свойство __proto__.
