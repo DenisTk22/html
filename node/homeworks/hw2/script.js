be6ab1579d0d569615b5ba9b919311920a836a28
Denis Tkachev <allpost-s@ya.ru>
2024-03-20 07:50:04 +0300

    algoritm

---
+++ node/homeworks/hw2/script.js
@@ -0,0 +1,7 @@
+
+const whatCanIDoNowArr = ['Включить любимую группу', 'Посмотреть на небо', 'Встать и пройтись', 'Позвонить другу', 'Позвонить маме', 'Поиграть в игру', 'Вспомнить крутой момент из своей жизни', 'Лечь спать', 'Погладить котика', 'Потянуться'];
+
+
+let whatCanIDoNow = whatCanIDoNowArr[Math.floor(Math.random()*whatCanIDoNowArr.length)];
+
+console.log(whatCanIDoNow);
