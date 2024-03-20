fb0f1f2f1db1b258eef7d9f579375fc6bddffc7e
Denis Tkachev <allpost-s@ya.ru>
2024-03-20 22:11:42 +0300

    npm project

--- node/homeworks/hw2/script.js
+++ node/homeworks/hw2/script.js
@@ -1,9 +1,13 @@
 
-const whatCanIDoNowArr = ['Включить любимую музыкальную группу', 'Посмотреть на небо', 'Встать и пройтись', 'Позвонить другу', 'Позвонить маме', 'Поиграть в игру', 'Вспомнить крутой момент из своей жизни', 'Лечь спать', 'Погладить котика', 'Потянуться', 'Сделать добро', 'Глубоко вдохнуть и расслабиться'];
+// const whatCanIDoNowArr = ['Включить любимую музыкальную группу', 'Посмотреть на небо', 'Встать и пройтись', 'Позвонить другу', 'Позвонить маме', 'Поиграть в игру', 'Вспомнить крутой момент из своей жизни', 'Лечь спать', 'Погладить котика', 'Потянуться', 'Сделать добро', 'Глубоко вдохнуть и расслабиться'];
 
 
-const whatCanIDoNow = () => {
-    return whatCanIDoNowArr[Math.floor(Math.random()*whatCanIDoNowArr.length)];
-}
+// const whatCanIDoNow = () => {
+//     return whatCanIDoNowArr[Math.floor(Math.random()*whatCanIDoNowArr.length)];
+// }
 
-console.log(whatCanIDoNow());
+// console.log(whatCanIDoNow());
+
+const wcid = require('whatcanido2024');
+
+console.log(wcid);
