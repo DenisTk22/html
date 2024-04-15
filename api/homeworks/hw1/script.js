9bfd4222f806441c73cf08c1549a9acf6a7f8e43
Denis Tkachev <allpost-s@ya.ru>
2024-04-15 13:46:01 +0300

    hw1 is done

--- api/homeworks/hw1/script.js
+++ api/homeworks/hw1/script.js
@@ -24,14 +24,16 @@
 Сохраняйте данные в LocalStorage, чтобы они сохранялись и отображались при перезагрузке страницы.
 
 Начальные данные (JSON):
+ */
 
+const scheduleData =
 [
     {
         "id": 1,
         "name": "Йога",
         "time": "10:00 - 11:00",
         "maxParticipants": 15,
-        "currentParticipants": 8
+        "currentParticipants": 15
     },
     {
         "id": 2,
@@ -61,5 +63,91 @@
         "maxParticipants": 8,
         "currentParticipants": 6
     }
-]
- */
+];
+
+// localStorage.clear();
+
+const scheduleKey = 'schedule';
+const schedule = getLsSchedule();
+const scheduleEl = document.querySelector('.schedule');
+
+
+if (!localStorage.getItem(scheduleKey)) {
+    localStorage.setItem(scheduleKey, JSON.stringify(scheduleData));
+};
+
+// получение расписания из LS
+function getLsSchedule() {
+    return JSON.parse(localStorage.getItem(scheduleKey));
+};
+// сохранение расписания в LS
+function saveSchedule(schedule) {
+    localStorage.setItem(scheduleKey, JSON.stringify(schedule));
+}
+// макет урока в HTML
+function getLessonHtml(lesson, noRecord) {
+    return `
+        <div class="lesson" data-id="${lesson.id}">
+            <h3 class="name">Занятие: ${lesson.name}</h3>
+            <p class="time">Время проведения: ${lesson.time}</p>
+            <p class="maxParticipants">Максимальное количество участников: ${lesson.maxParticipants}</p>
+            <p class="currentParticipants">Текущее количество участников: ${lesson.currentParticipants}</p>
+            <button class="getLesson" ${noRecord}>Записаться</button>
+            <button class="cancelLesson" disabled>Отменить запись</button>
+       </div>
+        `;
+}
+
+// вывод расписания
+schedule.forEach(lesson => {
+    if (lesson.currentParticipants >= lesson.maxParticipants) {
+        scheduleEl.insertAdjacentHTML('beforeend', getLessonHtml(lesson, 'disabled'));
+    } else {
+        scheduleEl.insertAdjacentHTML('beforeend', getLessonHtml(lesson, null));
+    }
+});
+
+scheduleEl.addEventListener('click', ({target}) => {
+    // запись на урок
+    if (target.closest('.getLesson')) {
+        const lessonEl = target.closest('.lesson');
+
+        const lessonId = lessonEl.dataset.id;
+        const schedule = getLsSchedule();
+        const editingLesson = schedule.find(lesson => lesson.id === Number(lessonId));
+
+        editingLesson.currentParticipants = Number(editingLesson.currentParticipants) + 1;
+
+        if (Number(editingLesson.currentParticipants) > Number(editingLesson.maxParticipants)) {
+            alert("Записаться нельзя!");
+            return;
+        }
+
+        saveSchedule(schedule);
+
+        lessonEl.querySelector('.currentParticipants').textContent = 
+        `Текущее количество участников: ${editingLesson.currentParticipants}`;
+
+        lessonEl.querySelector('.cancelLesson').disabled = false;
+        lessonEl.querySelector('.getLesson').disabled = true;
+    }
+
+    // отмена записи
+    if (target.closest('.cancelLesson')) {
+        const lessonEl = target.closest('.lesson');
+        const lessonId = lessonEl.dataset.id;
+        const schedule = getLsSchedule();
+
+        const editingLesson = schedule.find(lesson => lesson.id === Number(lessonId));
+
+        editingLesson.currentParticipants = Number(editingLesson.currentParticipants) - 1;
+
+        saveSchedule(schedule);
+
+        lessonEl.querySelector('.currentParticipants').textContent = 
+        `Текущее количество участников: ${editingLesson.currentParticipants}`;
+
+        lessonEl.querySelector('.cancelLesson').disabled = true;
+        lessonEl.querySelector('.getLesson').disabled = false;
+    }
+})
