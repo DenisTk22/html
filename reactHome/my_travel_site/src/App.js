3bffe98113bd5c46751ec79c807224a490191680
Denis Tkachev <allpost-s@ya.ru>
2024-06-18 14:26:29 +0300

    css

--- reactHome/my_travel_site/src/App.js
+++ reactHome/my_travel_site/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './css/style.css'
 import MainPage from './components/MainPage';
 import AboutPage from './components/AboutPage';
 import MotivationPage from './components/MotivationPage';
@@ -7,11 +8,14 @@ import Footer from './components/Footer';
 import AltaiText1 from './texts/AltaiText1';
 import AltaiText2 from './texts/AltaiText2';
 import KareliaText from './texts/KareliaText';
+import SolovkiText from './texts/SolovkiText';
+
 
 function App() {
   const altaiText1 = <AltaiText1/>
   const altaiText2 = <AltaiText2/>
   const kareliaText = <KareliaText/>
+  const solovkiText = <SolovkiText/>
 
   return (
     <div className="container center">
@@ -21,8 +25,8 @@ function App() {
         <MotivationPage id='motivation' />
         <RegionOfTravel id='altai' title='Алтай' text1={altaiText1} text2={altaiText2} />
         <RegionOfTravel id='karelia' title='Карелия' text1={kareliaText} text2={null} />
-        <RegionOfTravel />
-        <Footer />
+        <RegionOfTravel id='solovki' title='Соловки' text1={solovkiText} text2={null} />
+        <Footer id='footer' />
       </div>
     </div>
   );
--- reactHome/my_travel_site/src/components/Footer.js
+++ reactHome/my_travel_site/src/components/Footer.js
@@ -1,5 +1,35 @@
-export default function Footer(props) {
+export default function Footer({id}) { //footer
     return (
-        <></>
-    )
+        <footer id={id} class={id}>
+            <nav className={`${id}__nav`}>
+                <ul className={`${id}__nav_links`}>
+                    <li>
+                        <a className={`${id}__nav_link`} href="#main">На главную</a>
+                    </li>
+                    <li>
+                        <a className={`${id}__nav_link`} href="#about">Обо мне</a>
+                    </li>
+                    <li>
+                        <a className={`${id}__nav_link`} href="#motivation">Мотивация</a>
+                    </li>
+                </ul>
+                <ul className={`${id}__nav_contacts`}>
+                    <li>
+                        <a className={`${id}__nav_contact`}
+                            target="_blank"
+                            href="https://t.me/+_K03Uly8P7A0MGI6"
+                        >Мой канал в телеграм</a>
+                    </li>
+                    <li>
+                        <a className={`${id}__nav_contact`}
+                            target="_blank"
+                            href="https://vk.com/dgsined"
+                        >Моя страница Вконтакте</a>
+                    </li>
+                    <li className={`${id}__nav_email`}>Моя почта: allpost-s@ya.ru</li>
+                </ul>
+            </nav>
+            <p class={`${id}__createdBy`}>2024 год</p>
+        </footer>
+    );
 }
